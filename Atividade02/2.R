ea0c5462dfc1bebee53c2ff4b717752edac49487
gabriel-braga-uc <126345318+gabriel-braga-uc@users.noreply.github.com>
2024-04-25 17:15:47 -0300

    2
    2

--- Atividade02/2.R
+++ Atividade02/2.R
@@ -1,5 +1,5 @@
-#EXC02 A, B, C
-Y <- matrix(data=seq(1, 100), nrow=10, ncol=10, byrow=T)
-sum(Y[,seq(from=2, to=10, by=2)])
-sum(Y[seq(from=2, to=10, by=2),])
-sum(Y[seq(from=2, to=10, by=2),seq(from=2, to=10, by=2)])
+# EXC02 A, B, C
+Y <- matrix(data = seq(1, 100), nrow = 10, ncol = 10, byrow = T)
+sum(Y[, seq(from = 2, to = 10, by = 2)])
+sum(Y[seq(from = 2, to = 10, by = 2), ])
+sum(Y[seq(from = 2, to = 10, by = 2), seq(from = 2, to = 10, by = 2)])
---
+++ atividade06/1.R
@@ -0,0 +1,4 @@
+help(read)
+
+iris.table <- read.table("csv\\iris.csv", header=TRUE, sep=",")
+iris.table
