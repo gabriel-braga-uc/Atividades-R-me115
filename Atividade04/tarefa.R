13def123a8c7ff32b32da0337ad857205a2a73e9
gabriel-braga-uc <126345318+gabriel-braga-uc@users.noreply.github.com>
2024-05-27 11:48:31 -0300

    f
    f

--- Atividade04/tarefa.R
+++ Atividade04/tarefa.R
@@ -1,5 +1,4 @@
-#Atividade Fatores#
-
+#Atividade 04#
 
 #1
 my.matrix <- matrix(data = c(6, 34, 923, 5, 0, 112:116, 5, 9, 34, 76, 2, 545:549), nrow = 5)
@@ -35,4 +34,46 @@ sapply(list.objects, typeof)
 
 
 #4
-Titanic
+
+#a
+apply(Titanic, 2, sum)
+
+#b, c
+#?
+
+
+#5
+
+#a
+first <- matrix(38:67, 3)
+second <- matrix(56:91, 3)
+third <- matrix(82:144, 3)
+fourth <- matrix(46:93, 6)
+listobj <- list(first, second, third, fourth)
+
+#b
+lapply(listobj, "[", ,2)
+
+#c
+lapply(listobj, "[", 3,)
+
+
+#6
+iris
+
+#a
+sapply(iris, typeof)
+#Pelo resultado vemos que a sobre espécies é a não numérica.
+
+#b
+levels(iris$Species)
+
+#c
+mapply(unique, iris)
+
+
+#7
+PlantGrowth
+tapply(PlantGrowth$weight, PlantGrowth$group, sd)
+
+#Desafio
